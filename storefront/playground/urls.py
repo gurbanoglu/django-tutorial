5aaff730fd40b706c38723d7659abd4dedd98d4e
gurbanoglu <56699217+gurbanoglu@users.noreply.github.com>
2024-09-11 01:47:46 +0400

    Delete storefront directory

--- storefront/playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# URLConf (URL Configuration)
-urlpatterns = [
-  # Routes should always end with forward slash.
-  path('hello/', views.say_hello)
-]
